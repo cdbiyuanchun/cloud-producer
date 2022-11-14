440148b56ace159e2fb329a6ff45341f3b3bec47
cdbiyuanchun <117974321+cdbiyuanchun@users.noreply.github.com>
2022-11-14 12:59:54 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,18 +3,23 @@ package com.example.demo.controller;
 import com.example.demo.pojo.User;
 import com.example.demo.sevice.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
+    @Resource
     private UserService userService;
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
 
     @RequestMapping("/find/{id}")
     public User findUserById(@PathVariable(value = "id")Integer id){
@@ -33,9 +38,28 @@ public class UserController {
     public int deleteById(@PathVariable(value = "id") Integer id) {
         return userService.deleteById(id);
     }
-
-
-
+    @RequestMapping("/set-cache-users")
+    public String setCacheUsers() {
+        List<User> allUser = userService.findAll();
+        redisTemplate.opsForValue().set("allUser",allUser);
+        return "ok";
+    }
+    @RequestMapping("/get-cache-users")
+    public List<User> getCacheUsers() {
+        List<User> allUser1 = (List<User>)redisTemplate.opsForValue().get("allUser");
+        return allUser1;
+    }
+    @RequestMapping("/set-cache-user")
+    public String setCacheUser(@PathVariable(value = "id") Integer id) {
+        User user = userService.findUserById(id);
+        redisTemplate.opsForValue().set("user"+id,user,10, TimeUnit.SECONDS);
+        return "ok";
+    }
+    @RequestMapping("/get-cache-user")
+    public User getCacheUser(@PathVariable(value = "id") Integer id) {
+        User user = (User) redisTemplate.opsForValue().get("user"+id);
+        return user;
+    }
 
 
 
