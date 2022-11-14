5f7c3f678b76b0d8e5bbc1ba214c019bb960260b
cdbiyuanchun <117974321+cdbiyuanchun@users.noreply.github.com>
2022-11-14 13:57:25 +0800

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -21,6 +21,9 @@ public class UserController {
     @Resource
     private UserService userService;
     @Resource
+    private UserDao userDao;
+
+    @Resource
     private RedisTemplate<String,Object> redisTemplate;
 
     @RequestMapping("/find/{id}")
